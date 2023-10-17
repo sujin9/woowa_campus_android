340c3d08d94012f77cca7348464a8c0c23e9c3c7
Sujin Kim <sujinkim919@gmail.com>
2023-10-17 17:56:13 +0900

    build: 의존성 범위 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
-    testImplementation(project(mapOf("path" to ":network")))
+    implementation(project(":network"))
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
