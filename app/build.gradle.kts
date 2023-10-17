5435a7fe1a908a3cfc3faf15209668a031a56237
Sujin Kim <sujinkim919@gmail.com>
2023-10-17 16:47:52 +0900

    build: ktor codegen setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("kotlinx-serialization")
 }
 
 android {
@@ -61,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
+    testImplementation(project(mapOf("path" to ":network")))
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -71,7 +73,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     // serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+//    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3") // retrofit2 -> ktor
     // navigation
     implementation("androidx.navigation:navigation-compose:2.7.4")
     // material
@@ -85,5 +87,12 @@ dependencies {
     testImplementation("io.kotest:kotest-framework-engine-jvm:5.7.2")
     // reflection
     testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
-
+    // ktor
+    implementation("io.ktor:ktor-client-core:2.1.3")
+    implementation("io.ktor:ktor-client-cio:2.1.3")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.3")
+    // gson
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("io.ktor:ktor-client-gson:2.1.3")
+    implementation("io.ktor:ktor-serialization-gson:2.1.3")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,39 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.openapi.generator") version "7.0.1" apply true
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+}
+
+buildscript {
+    repositories {
+        maven(url = "https://repo1.maven.org/maven2")
+    }
+}
+
+tasks.register(
+    "generateClient",
+    org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class,
+) {
+    generatorName.set("kotlin")
+    // .yaml 파일이 위치하는 경로
+    inputSpec.set("$rootDir/app/src/test/java/woowacourse/campus/TestApi.yaml")
+    // generate된 코드가 위치할 경로
+    outputDir.set("$rootDir/network")
+    // genertate된 코드가 가질 상위 경로
+    packageName.set("")
+    apiPackage.set("api")
+    modelPackage.set("model")
+    invokerPackage.set("invoker")
+    // 설정하기
+    configOptions.set(
+        mapOf(
+            "library" to "jvm-ktor",
+            "dateLibrary" to "java8",
+            "omitGradleWrapper" to "true",
+            "sourceFolder" to "src/main/java",
+            "useCoroutines" to "true",
+            "serializationLibrary" to "gson"
+        ),
+    )
 }
