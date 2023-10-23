2e6129d1eb5fad54b7faf09dfb71a533d9fcb724
Sujin Kim <sujinkim919@gmail.com>
2023-10-23 17:14:06 +0900

    build: font system setting

--- app/src/main/java/woowacourse/campus/ui/theme/Type.kt
+++ app/src/main/java/woowacourse/campus/ui/theme/Type.kt
@@ -2,33 +2,30 @@ package woowacourse.campus.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import woowacourse.campus.R
 
 // Set of Material typography styles to start with
+private val hannaFontFamily = FontFamily(
+    Font(resId = R.font.bm_hanna_pro, weight = FontWeight.Bold),
+    Font(resId = R.font.bm_hanna_air, weight = FontWeight.Light),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    titleMedium = TextStyle(
+        fontFamily = hannaFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Normal,
+        fontSize = 18.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleSmall = TextStyle(
+        fontFamily = hannaFontFamily,
+        fontWeight = FontWeight.Light,
+        fontStyle = FontStyle.Normal,
+        fontSize = 16.sp,
     )
-    */
 )
