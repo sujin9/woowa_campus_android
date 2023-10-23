d0a0174fd87f7807a1c1fa883011defeeaa73063
Sujin Kim <sujinkim919@gmail.com>
2023-10-23 17:22:20 +0900

    feat: whole app에 background color 적용

--- app/src/main/java/woowacourse/campus/MainActivity.kt
+++ app/src/main/java/woowacourse/campus/MainActivity.kt
@@ -3,9 +3,12 @@ package woowacourse.campus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,11 +32,18 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun MainScreenView(navController: NavHostController = rememberNavController()) {
+internal fun MainScreenView(
+    navController: NavHostController = rememberNavController(),
+) {
     Scaffold(
         bottomBar = { BottomNavigationView(navController = navController) }
     ) {
-        Box(Modifier.padding(it)) {
+        Box(
+            Modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
             NavigationGraph(navController = navController)
         }
     }
