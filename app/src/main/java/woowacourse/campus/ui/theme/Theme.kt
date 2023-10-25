6785a228442d60628a0be87197bcee2a7ffd2f61
Sujin Kim <sujinkim919@gmail.com>
2023-10-25 15:24:19 +0900

    build: color theme 적용

--- app/src/main/java/woowacourse/campus/ui/theme/Theme.kt
+++ app/src/main/java/woowacourse/campus/ui/theme/Theme.kt
@@ -35,16 +35,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun WoowaCampusTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
